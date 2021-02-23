54ede33220fe23920ca1acf7bb67172258cead3d
Felipe Tommaselli <f.tommaselli@usp.br>
2021-02-23 11:22:25 -0300

    Add files via upload

---
+++ 14_Contatos/mainCONTATOS_14.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+typedef struct{
+	char nome[20];
+	unsigned int numero; 
+}contato;
+
+
+int main(int argc, char *argv[]) {
+contato usuario[5];
+
+	return 0;
+}
